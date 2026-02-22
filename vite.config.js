7a3344b65a69856bf5a43798f39835615a424420
Seu Nome <seu@email.com>
2026-02-22 07:53:18 -0300

    Agora funciona no Github tbm

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Garante que o site funcione em subpastas ou no GitHub Pages
 })
